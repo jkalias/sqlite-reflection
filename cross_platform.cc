929ca63b3e39ceef3e202ff2b5426f0777da1baa
jkalias <john@kaliakatsos.net>
2023-06-28 17:08:09 +0200

    fixed cross_platform.cc for macOS

--- cross_platform.cc
+++ cross_platform.cc
@@ -1,19 +1,21 @@
 #include "cross_platform.h"
 
 #include <codecvt>
-#ifndef __unix__
+#ifdef __unix__
+#include <locale>
+#else
 #include <windows.h>
 #endif
 
 namespace cross_platform {
 	std::wstring FromUtf8(const std::string& utf8_string) {
-		std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+		std::wstring_convert<std::codecvt_utf8<wchar_t> > converter;
 		auto wide_string = converter.from_bytes(utf8_string.data());
 		return wide_string;
 	}
 
 	std::string ToUtf8(const std::wstring& wide_string) {
-		std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+		std::wstring_convert<std::codecvt_utf8<wchar_t> > converter;
 		auto utf8_string = converter.to_bytes(wide_string.data());
 		return utf8_string;
 	}
@@ -21,7 +23,7 @@ namespace cross_platform {
 	int ToInt(const wchar_t* str) {
 #ifdef __unix__
 		std::string sstr = ToUtf8(str);
-		char* tmp = sstr.data();
+		auto tmp = sstr.data();
 		return atoi(tmp);
 #else
 		return _wtoi(str);
@@ -31,7 +33,7 @@ namespace cross_platform {
 	double ToDouble(const wchar_t* str) {
 #ifdef __unix__
 		std::string sstr = ToUtf8(str);
-		char* tmp = sstr.data();
+		auto tmp = sstr.data();
 		return atof(tmp);
 #else
 		return _wtof(str);
